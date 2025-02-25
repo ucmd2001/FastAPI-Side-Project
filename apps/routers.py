ed1fd25d36937374b81c81c09520703037f3824a
William <william@roboadvisor.com.tw>
2025-02-25 17:23:55 +0800

    tweak: adjust function to async, tweak: change index to using forloop, tweak: adjust home api

--- apps/routers.py
+++ apps/routers.py
@@ -6,10 +6,17 @@ runtime_settings = get_settings()
 
 routers = APIRouter(prefix=runtime_settings.HOME_URL)
 
+
 @routers.get("/")
+def home():
+    """home url"""
+    return {"message": "You're in Home, Welcome"}
+
+
 @routers.get("/Ping")
 def simple_healthcheck():
     """simple healthcheck"""
     return {"Ping": "Pong🏓 v0.0.1"}
+
 
 routers.include_router(plan_cart_router)
